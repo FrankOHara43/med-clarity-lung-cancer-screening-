71924099530976adb1b90a1f17f3f9c83c4bfccc
FrankOHara43 <aryaswaraj5@gmail.com>
2026-03-18 22:30:01 +0530

    Fix missing frontend dependencies for Render build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
-interface ImportMetaEnv {
-	readonly VITE_APP_NAME?: string;
-	readonly VITE_API_URL?: string;
-	readonly [key: string]: string | undefined;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
+/// <reference types="vite/client" />
